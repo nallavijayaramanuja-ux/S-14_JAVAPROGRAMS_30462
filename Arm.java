53df446f08432ebdfbce82e298ebf6efc6e1b2d1
nallavijayaramanuja-ux <nallavijayaramanuja@klh.edu.in>
2026-09-24 15:06:08 +0530

    Add files via upload

---
+++ Arm.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+		Class Arm
+		public Static void main (String args (){
+  		{
+
+		int no, temp,rev=0
+
+		Scanner Sc = new Scanner (system.in);
+		system.out.println (" please enter the number);
+		no= Sc.nextInt();
+		while(no>0)
+		{
+		x=no%10;
+		rev=rev+(x*x*x);
+		no=no/10;
+		
+		}
+
+		if (rev==temp)
+		 system.out.println("the entered number " + " temp" + is amstorng };
+		esle 
+		system.out.println{"the entered number " + " temp"+ is not amstrong }"
+
+  	 }
+} 
+
